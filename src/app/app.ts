62370661e97928f1451179339a61b0fc6bbf8faf
Mirzonom <77639035+Mirzonom@users.noreply.github.com>
2025-08-30 22:07:47 +0500

    init (tailwind, lucid)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, signal} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('reword-web');
+  vocabulary = [
+    { german: 'Hallo', russian: 'Привет' },
+    { german: 'Welt', russian: 'Мир' },
+    { german: 'Haus', russian: 'Дом' },
+    { german: 'Buch', russian: 'Книга' },
+    { german: 'Auto', russian: 'Машина' },
+    { german: 'Freund', russian: 'Друг' },
+    { german: 'Essen', russian: 'Еда' },
+    { german: 'Wasser', russian: 'Вода' },
+    { german: 'Schule', russian: 'Школа' },
+    { german: 'Arbeit', russian: 'Работа' },
+    { german: 'Liebe', russian: 'Любовь' },
+    { german: 'Zeit', russian: 'Время' }
+  ];
 }
